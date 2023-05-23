29bec92fb248f564c116035c7a53ad6d72a45fe5
Albert Shu <albert94shu@gmail.com>
2023-05-23 14:47:13 -0500

    Showing the Image

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,4 +1,38 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', async () => {
+    const url = 'http://localhost:8000/api/conferences/';
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Error getting data from API.');
+
+        } else {
+            const data = await response.json();
+            const conference = data.conferences[0];
+            const nameTag = document.querySelector('.card-title');
+            nameTag.innerHTML = conference.name;
+
+            const detailUrl = `http://localhost:8000${conference.href}`;
+            const detailResponse = await fetch(detailUrl);
+            if (detailResponse.ok) {
+                const details = await detailResponse.json();
+                const description = details.conference.description
+                const descriptionTag = document.querySelector('.card-text');
+                descriptionTag.innerHTML = description;
+                console.log(details)
+
+                const imageTag = document.querySelector('.card-img-top');
+                imageTag.src = details.conference.location.picture_url;
+
+
+            }
+
+
+        }
+
+    } catch (e) {
+        console.error(e)
+
+    }
 
 
 });
